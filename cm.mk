dff839334c36d3bde2c1dab72da02a13a63bd524
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-22 16:50:25 +0530

    Update
    Remove useless stuff. Restructure

--- cm.mk
+++ cm.mk
@@ -1,39 +1,24 @@
-# Prepare for new BootAnimation
-TARGET_BOOTANIMATION_NAME := vertical-240x320
-
-# Specify phone tech before including full_phone
-
 # Include GSM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Inherit from those products. Most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Release name and versioning
+PRODUCT_RELEASE_NAME := totoro
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+# Prepare for new BootAnimation
+TARGET_BOOTANIMATION_NAME := vertical-240x320
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/totoro/device_totoro.mk)
+$(call inherit-product, device/samsung/totoro/full_totoro.mk)
 
-# Include FM-Radio stuff
-#$(call inherit-product, vendor/cm/products/bcm_fm_radio.mk)
-
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_totoro
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := totoro
 PRODUCT_MODEL := GT-S5360
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := phone
 
-# Device identifier. This must come after all inclusions
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=totoro \
-    BUILD_FINGERPRINT="samsung/totoro/totoro:4.1.2/GRI40/19C11F164C:user/release-keys" \
-    PRIVATE_BUILD_DESC="totoro-user 4.1.2 GRI40 19C11F164C release-keys"
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := totoro
-PRODUCT_VERSION_DEVICE_SPECIFIC := SGY
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=totoro TARGET_DEVICE=totoro
